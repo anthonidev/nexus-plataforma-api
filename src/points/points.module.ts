ec24f7639da37f97bc80cd4433e0eadc02439990
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-04-08 17:21:17 -0500

    update module rank

--- src/points/points.module.ts
+++ src/points/points.module.ts
@@ -1,28 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PointsTransaction } from './entities/points_transactions.entity';
-import { RankHistory } from './entities/rank_history.entity';
-import { Rank } from './entities/ranks.entity';
 import { UserPoints } from './entities/user_points.entity';
-import { UserRank } from './entities/user_ranks.entity';
 import { WeeklyVolume } from './entities/weekly_volumes.entity';
 import { PointsController } from './controllers/points.controller';
 import { PointsService } from './services/points.service';
 import { UserModule } from 'src/user/user.module';
-import { MonthlyVolumeRank } from './entities/monthly_volume_ranks.entity';
 
 @Module({
   controllers: [PointsController],
   imports: [
-    TypeOrmModule.forFeature([
-      PointsTransaction,
-      RankHistory,
-      Rank,
-      UserPoints,
-      UserRank,
-      WeeklyVolume,
-      MonthlyVolumeRank,
-    ]),
+    TypeOrmModule.forFeature([PointsTransaction, UserPoints, WeeklyVolume]),
     UserModule,
   ],
   providers: [PointsService],
--- src/ranks/ranks.module.ts
+++ src/ranks/ranks.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RanksService } from './ranks.service';
 import { RanksController } from './ranks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rank } from './entities/ranks.entity';
+import { UserRank } from './entities/user_ranks.entity';
+import { MonthlyVolumeRank } from './entities/monthly_volume_ranks.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [RanksController],
   providers: [RanksService],
+  imports: [
+    TypeOrmModule.forFeature([Rank, UserRank, MonthlyVolumeRank]),
+    UserModule,
+  ],
+  exports: [RanksService, TypeOrmModule],
 })
 export class RanksModule {}
