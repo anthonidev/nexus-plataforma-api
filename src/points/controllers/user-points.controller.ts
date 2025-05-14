df68e5968dd6abd0678fe87af28a94728d7a988b
JosmenDev <josmendev@gmail.com>
2025-05-14 15:37:37 -0500

    feat(user-points): implemented method to find one transaction points to user and transaction payments to user

--- src/points/controllers/user-points.controller.ts
+++ src/points/controllers/user-points.controller.ts
@@ -1,15 +1,40 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe, Query } from "@nestjs/common";
 import { UserPointsService } from "../services/user-points.service";
-import { PaginationDto } from "src/common/dto/paginationDto";
 import { GetUserPointsDto } from "../dto/get-user-points.dto";
+import { FindPointsTransactionDto } from "../dto/find-weekly-volume.dto";
+import { PaginationDto } from "src/common/dto/paginationDto";
 
 @Controller('user-points')
 export class UserPointsController {
   constructor(private readonly userPointsService: UserPointsService) {}
-  @Get()
+  @Get('list')
   getUserPoints(
     @Query() getUsetPointsDto: GetUserPointsDto,
   ) {
     return this.userPointsService.getUserPoints(getUsetPointsDto);
+  }
+
+  @Get('transactions/:id')
+  getUserPointsTransactions(
+    @Param('id') id: string,
+    @Query() filters: FindPointsTransactionDto,
+  ) {
+    return this.userPointsService.getUserPointsTransactions(
+      id,
+      filters,
+    );
+  }
+
+  @Get('transactions/:id/payments/:userId')
+  getUserPointsTransactionPayments(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('userId') userId: string,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    return this.userPointsService.getUserPointsTransactionPayments(
+      id,
+      userId,
+      paginationDto,
+    );
   }
 }
--- src/points/services/user-points.service.ts
+++ src/points/services/user-points.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserPoints } from "../entities/user_points.entity";
 import { Repository } from "typeorm";
 import { PaginationDto } from "src/common/dto/paginationDto";
 import { PaginationHelper } from "src/common/helpers/pagination.helper";
 import { GetUserPointsDto } from '../dto/get-user-points.dto';
+import { FindPointsTransactionDto } from "../dto/find-weekly-volume.dto";
+import { PointsTransaction } from "../entities/points_transactions.entity";
+import { User } from "src/user/entities/user.entity";
+import { PointsTransactionPayment } from "../entities/points-transactions-payments.entity";
 
 @Injectable()
 export class UserPointsService {
+  private readonly logger = new Logger(UserPointsService.name);
   constructor(
     @InjectRepository(UserPoints)
     private readonly userPointsRepository: Repository<UserPoints>,
+    @InjectRepository(PointsTransaction)
+    private readonly pointsTransactionRepository: Repository<PointsTransaction>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
   async getUserPoints(
     getUserPointsDto: GetUserPointsDto,
@@ -36,6 +45,10 @@ export class UserPointsService {
         },
       );
 
+    queryBuilder
+      .skip((page - 1) * limit)
+      .take(limit);
+
     const [items, totalItems] = await queryBuilder.getManyAndCount();
     return PaginationHelper.createPaginatedResponse(
       items,
@@ -43,4 +56,118 @@ export class UserPointsService {
       paginationDto,
     );
   }
+
+  async getUserPointsTransactions(
+    userId: string,
+    filters: FindPointsTransactionDto,
+  ) {
+    try {
+      const { page = 1, limit = 10 } = filters;
+      const queryBuilder = this.pointsTransactionRepository
+        .createQueryBuilder('transaction')
+        .leftJoin('transaction.user', 'user')
+        .where('user.id = :userId', { userId });
+
+      if (filters.type)
+        queryBuilder.andWhere('transaction.type = :type', { type: filters.type });
+      if (filters.status)
+        queryBuilder.andWhere('transaction.status = :status', { status: filters.status });
+      if (filters.startDate)
+        queryBuilder.andWhere('transaction.createdAt >= :startDate', {
+          startDate: new Date(filters.startDate),
+        });
+      if (filters.endDate) {
+        const endOfDay = new Date(filters.endDate);
+        endOfDay.setHours(23, 59, 59, 999);
+        queryBuilder.andWhere('transaction.createdAt <= :endDate', {
+          endDate: endOfDay,
+        });
+      }
+
+      queryBuilder
+        .orderBy('transaction.createdAt', filters.order)
+        .skip((page - 1) * limit)
+        .take(limit);
+
+      const [items, totalItems] = await queryBuilder.getManyAndCount();
+
+      const user = await this.userRepository.findOne({
+        where: { id: userId },
+        relations: ['personalInfo'],
+      });
+
+      return {
+        user,
+        transactions: PaginationHelper.createPaginatedResponse(
+          items,
+          totalItems,
+          filters,
+        )
+      };
+    } catch (error) {
+      this.logger.error(
+        `Error al obtener transacciones de puntos: ${error.message}`,
+      );
+      throw error;
+    }
+  }
+
+  async getUserPointsTransactionPayments(
+    id: number,
+    userId: string,
+    paginationDto: PaginationDto,
+  ) {
+      try {
+        const getPointsTransactionDetails = await this.pointsTransactionRepository.findOne({
+          where: { id, user: { id: userId } },
+          relations: [
+            'pointsTransactionsPayments',
+            'pointsTransactionsPayments.payment',
+          ],
+        });
+        if (!getPointsTransactionDetails)
+          throw new NotFoundException(`Transacción de puntos con ID ${id} no encontrada`);
+        const { pointsTransactionsPayments, ...restData } =getPointsTransactionDetails;
+        const pointsTransactionsPaymentsDetails = await this.paymentDetails(pointsTransactionsPayments);
+        const user = await this.userRepository.findOne({
+            where: { id: userId },
+            relations: ['personalInfo'],
+          });
+        return {
+          ...restData,
+          listPayments: PaginationHelper.createPaginatedResponse(
+            pointsTransactionsPaymentsDetails,
+            pointsTransactionsPaymentsDetails.length,
+            paginationDto,
+          ),
+          user,
+        }
+      } catch (error) {
+        this.logger.error(
+          `Error al obtener los pagos de la transacción de puntos: ${error.message}`,
+        );
+        throw error;
+      }
+  }
+
+  private async paymentDetails(
+      entity: PointsTransactionPayment[]
+    ) {
+      return entity.map(item => {
+        const { payment, ...restData } = item;
+        return {
+          ...restData,
+          payment: {
+            id: payment.id,
+            amount: payment.amount,
+            methodPayment: payment.methodPayment,
+            codeOperation: payment.codeOperation,
+            banckName: payment.banckName,
+            dateOperation: payment.dateOperation,
+            numberTicket: payment.numberTicket,
+            status: payment.status, 
+          },
+        };
+      });
+    }
 }
