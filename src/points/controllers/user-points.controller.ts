89547b65fa61e60dbbb03f4dc01e1ad15ae690ca
JosmenDev <josmendev@gmail.com>
2025-05-14 16:53:17 -0500

    feat(user-points): added authentication to the controller

--- src/points/controllers/user-points.controller.ts
+++ src/points/controllers/user-points.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe, Query, UseGuards } from "@nestjs/common";
 import { UserPointsService } from "../services/user-points.service";
 import { GetUserPointsDto } from "../dto/get-user-points.dto";
 import { FindPointsTransactionDto } from "../dto/find-weekly-volume.dto";
 import { PaginationDto } from "src/common/dto/paginationDto";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { RolesGuard } from "src/auth/guards/roles.guard";
+import { Roles } from "src/auth/decorators/roles.decorator";
 
 @Controller('user-points')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class UserPointsController {
   constructor(private readonly userPointsService: UserPointsService) {}
+
   @Get('list')
+  @Roles('FAC')
   getUserPoints(
     @Query() getUsetPointsDto: GetUserPointsDto,
   ) {
@@ -15,6 +21,7 @@ export class UserPointsController {
   }
 
   @Get('transactions/:id')
+  @Roles('FAC')
   getUserPointsTransactions(
     @Param('id') id: string,
     @Query() filters: FindPointsTransactionDto,
@@ -26,6 +33,7 @@ export class UserPointsController {
   }
 
   @Get('transactions/:id/payments/:userId')
+  @Roles('FAC')
   getUserPointsTransactionPayments(
     @Param('id', ParseIntPipe) id: number,
     @Param('userId') userId: string,
